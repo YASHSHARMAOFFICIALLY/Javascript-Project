e412d8444823b656b697886935709f45d53c610f
Yash sharma <yashsharmaofficially@gmail.com>
2025-10-23 23:35:15 +0530

    new feature

--- 03_randompasswordd/index.js
+++ 03_randompasswordd/index.js
@@ -34,3 +34,10 @@ document.querySelectorAll('.option input[type="checkbox"], button.generatebutton
   .forEach((elem) => {
       elem.addEventListener("click", generate);
   });
+document.querySelector(".copy").addEventListener("click", () => {
+  const password = document.querySelector(".display").value;
+  if (password) {
+    navigator.clipboard.writeText(password);
+    alert("Password copied to clipboard!");
+  }
+});
