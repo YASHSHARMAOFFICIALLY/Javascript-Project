3b8bb05387c6ee28d4e72af327b6abd567931a7a
Yash sharma <yashsharmaofficially@gmail.com>
2025-10-23 23:17:39 +0530

    Added random password project

---
+++ 03_randompasswordd/index.js
@@ -0,0 +1,36 @@
+function generate() {
+    let dictionary = "";
+    if (document.getElementById("lowercase").checked) {
+        dictionary += "qwertyuiopasdfghjklzxcvbnm";
+    }
+    if (document.getElementById("uppercase").checked) {
+        dictionary += "QWERTYUIOPASDFGHJKLZXCVBNM";
+    }
+    if (document.getElementById("number").checked) {
+        dictionary += "1234567890";
+    }
+    if (document.getElementById("symbol").checked) {
+        dictionary += "!@#$%^&*()_+-={}[];<>:";
+    }
+
+    const length = document.querySelector('.slider').value;
+
+
+    if (length < 1 || dictionary.length === 0) {
+        return;
+    }
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const pos = Math.floor(Math.random() * dictionary.length);
+        password += dictionary[pos]; 
+    }
+    document.querySelector('.display').value = password;
+}
+document.querySelector('.slider').addEventListener("input",(e) => {
+    document.querySelector("div.lengthh span").innerHTML = e.target.value;
+    generate()
+})
+document.querySelectorAll('.option input[type="checkbox"], button.generatebutton')
+  .forEach((elem) => {
+      elem.addEventListener("click", generate);
+  });
